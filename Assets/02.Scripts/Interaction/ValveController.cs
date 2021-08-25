ff189c5340f9550a9e96fc6279a6f6000f08de34
유상욱 <tkddnr032@gmail.com>
2021-08-25 18:49:41 +0900

    Update VR Binding

--- Assets/02.Scripts/Interaction/ValveController.cs
+++ Assets/02.Scripts/Interaction/ValveController.cs
@@ -5,55 +5,71 @@ using Valve.VR;
 
 public class ValveController : MonoBehaviour
 {
-    public SteamVR_Action_Vector2 ThumbstickAction = null;
-    public SteamVR_Action_Vector2 TrackpadAction = null;
-    public SteamVR_Action_Single SqueezeAction = null;
-    public SteamVR_Action_Boolean GripAction = null;
-    public SteamVR_Action_Boolean grabPinch = null;
-    public SteamVR_Action_Skeleton SkeletonAction = null;
-
     public SteamVR_Input_Sources handType;
+
+    public SteamVR_Action_Boolean interactUI = null;
+    public SteamVR_Action_Boolean teleport = null;
+    public SteamVR_Action_Boolean grabPinch = null;
+    public SteamVR_Action_Boolean grabGrip = null;
+    public SteamVR_Action_Boolean snapTurnRight = null;
+    public SteamVR_Action_Boolean snapTurnLeft = null;
 
     private void Update()
     {
-        //Thumbstick();
-        //Trackpad();
-        //Squeeze();
-        //Grip();
-        Pinch();
-        //Skeleton();
+        InteractUI();
+        Teleport();
+        GrabPinch();
+        GrabGrip();
+        SnapTurnRight();
+        SnapTurnLeft();
     }
 
-    private void Thumbstick()
+    private void InteractUI() // VR 컨트롤러 트리거 UI 인터렉션
     {
-
-    }
-
-    private void Trackpad()
-    {
-
-    }
-
-    private void Squeeze()
-    {
-
-    }
-
-    private void Grip()
-    {
-
-    }
-
-    private void Pinch()
-    {
-        if (grabPinch.GetState(handType))
+        if (interactUI.GetStateDown(handType))
         {
-            Debug.Log("트리거 누르는 중");
+            Debug.Log("InteractUI");
         }
     }
 
-    private void Skeleton()
+    private void Teleport() // VR 컨트롤러 트랙패드 (North, Center, South)
     {
+        if (teleport.GetStateDown(handType))
+        {
+            Debug.Log("Teleport");
+        }
+    }
 
+    private void GrabPinch() // VR 컨트롤러 트리거
+    {
+        if (grabPinch.GetState(handType))
+        {
+            Debug.Log("GrabPinch");
+        }
+    }
+
+    private void GrabGrip() // VR 컨트롤러 좌우버튼
+    {
+        if (grabGrip.GetState(handType))
+        {
+            Debug.Log("GrabGrip");
+        }
+    }
+
+    private void SnapTurnRight() // VR 컨트롤러 트랙패드 (East)
+    {
+        if (snapTurnRight.GetState(handType))
+        {
+            Debug.Log("snapTurnRight");
+        }
+    }
+
+    private void SnapTurnLeft() // VR 컨트롤러 트랙패드 (West)
+    {
+        if (snapTurnLeft.GetState(handType))
+        {
+            Debug.Log("snapTurnLeft");
+        }
     }
 }
+
